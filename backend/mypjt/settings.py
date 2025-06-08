4027474d6317f7ea290eaf056809ba1fbee5aa10
MYSBS <qjatnekt567@gmail.com>
2025-06-09 02:29:29 +0900

    csrf

--- backend/mypjt/settings.py
+++ backend/mypjt/settings.py
@@ -92,7 +92,7 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
-    # 'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -235,4 +235,18 @@ CORS_ALLOWED_ORIGINS = [
     'https://amuredo.me',
     'https://www.amuredo.me',
     'https://34.47.115.134'
+]
+
+CSRF_COOKIE_SECURE = True
+CSRF_COOKIE_SAMESITE = 'None'  # 크로스 도메인 환경에서 필수
+
+# CORS 설정
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOWED_ORIGINS = [
+    'https://amuredo.me',
+    'https://www.amuredo.me'
+]
+CSRF_TRUSTED_ORIGINS = [
+    'https://amuredo.me',
+    'https://www.amuredo.me'
 ]
